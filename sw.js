7efa736ddf96d1d765abcd3710077de8ed62f13a
Stas Razborskii <stasaktivniy@yandex.ru>
2024-09-28 20:58:08 +0300

    fix/ удалил библиотеку анимации mo.js, добавил файл service worker sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+// const CACHE_NAME = 'sw-app-cracker-v1.0.0'
+
+// const ASSETS_URL = [
+//   'index.html',
+//   '/pages/*',
+//   '/images/*',
+//   '/blocks/*',
+//   '/vendor/*'
+// ]
+
+// self.addEventListener('install', async event => {
+//   console.log('installed');
+//   const CACHE = await caches.open(CACHE_NAME)
+//   await CACHE.addAll(ASSETS_URL)
+// })
+
+// self.addEventListener('activate', event => {
+//   console.log('activated');
+// })
